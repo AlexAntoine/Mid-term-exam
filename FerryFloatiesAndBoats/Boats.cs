e873cadc3627740465f71a550f67b3a51ac5732f
AlexAntoine <antoinealex1234@gmial.com>
2021-04-27 08:21:28 -0400

    inital commit

---
+++ FerryFloatiesAndBoats/Boats.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FerryFloatiesAndBoats
+{
+    public class Boats: Ferry
+    {
+        protected string _houseBoatName;
+        protected int _beds;
+
+        public Boats()
+        {
+
+        }
+        public bool setBeds()
+        {
+            //take input, verfie it's an integer
+            //if it is return true convert input to an int
+            //and assign it to beds property, if it is not return false
+        }
+
+        public string getBeds()
+        {
+            //return beds properties as a string 
+        }
+
+        public bool setHouseBoatName()
+        {
+            /*
+             * take string input and assign it to houseBoatName property
+             * return true
+             */
+        }
+
+        public string getHouseBoatName()
+        {
+            /*
+             * return houseboadname as a string
+             */
+        }
+    }
+}
---
+++ FerryFloatiesAndBoats/Ferry.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FerryFloatiesAndBoats
+{
+    public class Ferry
+    {
+        protected string _FerryName;
+        protected int _passengerCap;
+        protected int _autoCap;
+        protected int _length;
+        protected int _draft;
+        protected int _width;
+        protected int _hp;
+        protected double _serviceSpeed;
+        protected string _homePort;
+
+        public bool setPassengerCap()
+        {
+           /*
+            * take string input, verifiy it is an integers,
+            * if true assign to passengerCap property and return true,
+            * if false return false
+            */
+        }
+
+        public string getPassengerCap()
+        {
+           /*
+            * return passengerCap property as a string 
+            */
+        }
+
+        public bool setAutoCap()
+        {
+            /*
+            * take string input, verifiy it is an integers,
+            * if true assign to autoCap property and return true,
+            * if false return false
+            */
+        }
+
+        public string getAutoCap()
+        {
+            /*
+           * return autoCap property as a string 
+           */
+        }
+
+        public bool setLength()
+        {
+            /*
+            * take string input, verifiy it is an integers,
+            * if true assign to length property and return true,
+            * if false return false
+            */
+        }
+
+        public string getLength()
+        {
+            /*
+           * return length property as a string 
+           */
+        }
+
+        public bool setDraft()
+        {
+            /*
+            * take string input, verifiy it is an integers,
+            * if true assign to draft property and return true,
+            * if false return false
+            */
+        }
+
+        public string getDraft()
+        {
+            /*
+           * return draft property as a string 
+           */
+        }
+
+        public bool setHomePort()
+        {
+           /*
+            * assign input to homeport property and return true
+            */
+        }
+
+        public string getHomePort()
+        {
+           //return homeport property
+        }
+
+        public bool setFerryName()
+        {
+            /*
+           * assign input to ferryName property and return true
+           */
+        }
+
+        public string getFerryName()
+        {
+            //return FerryName property
+        }
+
+        public bool setServiceSpeed()
+        {
+            /*
+            * take string input, verifiy it is a double,
+            * if true assign to serviceSpeed property and return true,
+            * if false return false
+            */
+        }
+
+        public string getServiceSpeed()
+        {
+            /*
+        * return serviceSpeed property as a string 
+        */
+        }
+
+        public bool setWidth()
+        {
+            /*
+            * take string input, verifiy it is an integers,
+            * if true assign to width property and return true,
+            * if false return false
+            */
+        }
+
+        public string getWidth()
+        {
+            /*
+           * return width property as a string 
+           */
+        }
+
+        public bool setHp()
+        {
+            /*
+             * take string input, verifiy it is an integers,
+             * if true assign to hp property and return true,
+             * if false return false
+             */
+        }
+
+        public string getHp()
+        {
+            /*
+           * return passengerCap hp property as a string 
+           */
+        }
+    }
+}
